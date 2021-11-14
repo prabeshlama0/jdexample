6b7c5a201ca542eb00d8e21cb3a3d3088a3a3821
Prabeshlama0@gmail.com <prabeshlama0@gmail.com>
2021-11-13 19:36:23 -0600

    Initial commit

---
+++ src/main/java/com/example/jdexample/JdexampleApplication.java
@@ -0,0 +1,26 @@
+package com.example.jdexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JdexampleApplication {
+
+
+
+	@GetMapping("/get")
+	public String get(){
+	return 	"Jenkins to Docker";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JdexampleApplication.class, args);
+
+
+
+
+	}
+
+}
